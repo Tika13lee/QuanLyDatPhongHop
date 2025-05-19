072bd32246957561498d4bae011c5448bb4fd266
Tika13lee <thuykieu130303@gmail.com>
2025-05-19 21:28:21 +0700

    edit

--- Backend/src/main/java/vn/com/kltn_project_v1/repositories/RequestFormRepository.java
+++ Backend/src/main/java/vn/com/kltn_project_v1/repositories/RequestFormRepository.java
@@ -2,6 +2,7 @@ package vn.com.kltn_project_v1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vn.com.kltn_project_v1.model.RequestForm;
 import vn.com.kltn_project_v1.model.StatusRequestForm;
@@ -18,9 +19,10 @@ public interface RequestFormRepository extends JpaRepository<RequestForm, Long> 
 
     @Query("SELECT r FROM RequestForm r WHERE ( :statusRequestForm is null or r.statusRequestForm = :statusRequestForm) and r.requestReservation.roomId = :roomId and (:typeRequestForm is null or r.typeRequestForm = :typeRequestForm) order by r.timeRequest desc ")
     List<RequestForm> findRequestFormByRoomId(long roomId,StatusRequestForm statusRequestForm, TypeRequestForm typeRequestForm);
-    @Query("SELECT r FROM RequestForm r WHERE  ( :statusRequestForm is null or r.statusRequestForm = :statusRequestForm) and (:typeRequestForm is null or r.typeRequestForm = :typeRequestForm) and (:dayStart is null or FUNCTION('DATE', r.timeRequest) = FUNCTION('DATE', dayStart)) order by r.timeRequest desc ")
-    List<RequestForm> findRequestFormByStatusPending(StatusRequestForm statusRequestForm, TypeRequestForm typeRequestForm,Date dayStart);
-    @Query("select r from RequestForm r join r.reservations res where res.reservationId = :reservationId and r.statusRequestForm = 'APPROVED' order by r.timeRequest desc ")
+    @Query("SELECT r FROM RequestForm r WHERE  (:statusRequestForm is null or r.statusRequestForm = :statusRequestForm) and (:typeRequestForm is null or r.typeRequestForm = :typeRequestForm) and (:dayStart is null or FUNCTION('DATE', r.timeRequest) = FUNCTION('DATE', :dayStart)) order by r.timeRequest desc ")
+    List<RequestForm> findRequestFormByStatusPending(@Param("statusRequestForm") StatusRequestForm statusRequestForm,
+                                                     @Param("typeRequestForm") TypeRequestForm typeRequestForm,
+                                                     @Param("dayStart") Date dayStart); @Query("select r from RequestForm r join r.reservations res where res.reservationId = :reservationId and r.statusRequestForm = 'APPROVED' order by r.timeRequest desc ")
     List<RequestForm> findRequestFormByReservationId(long reservationId);
 
 }
